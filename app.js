d3188ce07046a22159165fd9890a06189af41ca9
ranjitkc7 <ranjit252@gmail.com>
2025-07-12 17:08:09 +0545

    third-commit

--- app.js
+++ app.js
@@ -7,20 +7,17 @@ const app = express();
 const staticPath = path.join(import.meta.dirname, "public");
 app.use(express.static(staticPath));
 
-app.get("/profile/:username", (req, res) => {
-  console.log(req.params);
-  res.send(`<h1>Hello ${req.params.username}. Where are you from?<h1>`);
-});
-app.get("/profile/:username/article/:data", (req, res) => {
-  console.log(req.params);
-  const formattedData = req.params.data.replace(/-/g, " ");
-  res.send(`<h1> The article ${formattedData}  by ${req.params.username}.<h1>`);
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-app.get("/product", (req, res) => {
-  console.log(req.query);
-  res.send(`<h1>Product Page and our product is ${req.query.search} with
-    price ${req.query.price}.<h1>`);
+app.use((req, res) => {
+  return res
+    .status(404)
+    .sendFile(path.join(import.meta.dirname, "views/404.html"));  
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- zod.js
+++ zod.js
@@ -44,3 +44,18 @@
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
+// app.get("/profile/:username", (req, res) => {
+//   console.log(req.params);
+//   res.send(`<h1>Hello ${req.params.username}. Where are you from?<h1>`);
+// });
+// app.get("/profile/:username/article/:data", (req, res) => {
+//   console.log(req.params);
+//   const formattedData = req.params.data.replace(/-/g, " ");
+//   res.send(`<h1> The article ${formattedData}  by ${req.params.username}.<h1>`);
+// });
+
+// app.get("/product", (req, res) => {
+//   console.log(req.query);
+//   res.send(`<h1>Product Page and our product is ${req.query.search} with
+//     price ${req.query.price}.<h1>`);
+// });
